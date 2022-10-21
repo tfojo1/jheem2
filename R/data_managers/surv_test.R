a90bbfeeb43c9a50e16afcfa51f82c1f9e2e0f22
JPennington <jpennin6@jhu.edu>
2022-10-21 13:50:37 -0400

    Year Construction Function + Test

---
+++ R/data_managers/surv_test.R
@@ -0,0 +1,84 @@
+library(testit)
+
+source("surveillance_data_manager.R")
+
+test.header <- function (func_name) {
+    cat(sprintf("-- Running test suite for %s --\n", func_name))
+}
+
+test.header("get.years.for.year.value")
+
+#Test 1
+x = get.years.for.year.value(NA,"2001a")
+assert ("1. 2001a should return NA", is.na(x))
+
+#Test 2
+x = get.years.for.year.value(NA, "2018")
+assert ("2. String 2018 should convert easily", x == c(2018))
+
+#Test 3
+x = get.years.for.year.value(NA, "2018-2020")
+assert ("3. Valid date range 2018-2020", x == c(2018,2019,2020))
+
+#Test 4
+x = get.years.for.year.value(NA, "2020-2018")
+assert ("4. Invalid date range 2020-2018", is.na(x))
+
+#Test 5
+x = get.years.for.year.value(NA, "2020-2020")
+assert ("5. Valid date range 2020-2020", x == c(2020))
+
+#Test 6
+x = get.years.for.year.value(NA, "2018-2020a")
+assert ("6. Invalid date range 2018-2020a", is.na(x))
+
+#Test 7
+x = get.years.for.year.value(NA, "2018a-2020")
+assert ("7. Invalid date range 2018a-2020", is.na(x))
+
+#Test 8
+x = get.years.for.year.value(NA, c("2018","2019","2020"))
+assert ("8. Valid vector of character years", x == c(2018,2019,2020))
+
+#Test 9
+x = get.years.for.year.value(NA, c("2017","2019","2020"))
+assert ("9. Invalid vector of character years", is.na(x))
+
+#Test 10
+x = get.years.for.year.value(NA, c("2018","2020","2021"))
+assert ("10. Invalid vector of character years", is.na(x))
+
+#Test 11
+x = get.years.for.year.value(NA, 2019)
+assert ("11. Valid single double parameter", x == c(2019))
+
+#Test 12
+x = get.years.for.year.value(NA, c(2019))
+assert ("12. Valid single double vector parameter", x == c(2019))
+
+#Test 13
+x = get.years.for.year.value(NA, c(2019,2020,2021,2022))
+assert ("13. Valid multi double vector parameter", x == c(2019,2020,2021,2022))
+
+#Test 14
+x = get.years.for.year.value(NA, c(2019,2020,2021,2023))
+assert ("14. Invalid multi double vector parameter", is.na(x))
+
+#Test 15
+x = get.years.for.year.value(NA, 2019L)
+assert ("15. Valid single integer parameter", x == c(2019))
+
+#Test 16
+x = get.years.for.year.value(NA, c(2019L))
+assert ("16. Valid single integer vector parameter", x == c(2019))
+
+#Test 17
+x = get.years.for.year.value(NA, c(2019L,2020L,2021L,2022L))
+assert ("17. Valid multi integer vector parameter", x == c(2019,2020,2021,2022))
+
+#Test 18
+x = get.years.for.year.value(NA, c(2019L,2020L,2021L,2023L))
+assert ("18. Invalid multi integer vector parameter", is.na(x))
+
+
+cat(sprintf("-- All Tests Passed --\n"))
--- R/data_managers/surveillance_data_manager.R
+++ R/data_managers/surveillance_data_manager.R
@@ -1,4 +1,4 @@
-
+library(stringr)
 
 #'@return An array that is indexed [outcome, location, year, <other stratifying dimension>]
 #'
@@ -21,9 +21,84 @@ get.surveillance.data <- function(surveillance.data.manager,
 get.years.for.year.value <- function(surveillance.manager,
                                      year.value)
 {
+    # I believe this should take a value and convert it into a vector representation
+    # So if there is only one entry, we return it as c(entry).
+    # This way we have a consistent return value, a vector of values (possibly none, if not
+    # a year)
+
     # for example
-    # year.value = 2020 or '2020' -> 2020
+    # year.value = 2020 or '2020' or c(2020) -> c(2020)
     # year.value = "2018-2020" --> c(2018,2019,2020)
+    # year.value = 2018:2020 --> c(2018,2019,2020)
+    # year.value = c("2018","2019") --> c(2018,2019)
+    # year.value = c(2018, 2019, 2020) --> c(2018, 2019, 2020)
+
+    # If this function is unable to determine what years are being
+    # asked for it will return NA
+
+    # We only allow consequtive years; we assume that the numbers themselves
+    # are valid years, and we allow both integers and doubles
+
+    entry.length <- length(year.value)
+    type <- typeof(year.value)
+
+    rv <- NA
+    # String?
+    if (type == "character") {
+        if (entry.length == 1) {
+            # One String
+            # Split on -
+            dash.split = str_split(year.value, "-")[[1]]
+            dash.split.length = length(dash.split)
+            if (dash.split.length == 1) {
+                # At this point we are reasonably sure it's a single year,
+                # Let's try to return it.  Will return NA if not valid.
+                rv = c(suppressWarnings(as.numeric(year.value)))
+            } else if (dash.split.length == 2) {
+                # There is only one dash, we have a date range
+                years = suppressWarnings(as.numeric(dash.split))
+                if (!any(is.na(years))) {
+                    # We have valid years for both entries
+                    # Check if the first year is less than or equal to the second
+                    if (years[1] <= years[2]) {
+                        rv = years[1]:years[2]
+                    }
+                }
+            } 
+            # If dash split length is greater than 2, we have an 
+            # invalid range
+        } else {
+            # We have a vector of character years
+            # Check if numeric
+            years = suppressWarnings(as.numeric(year.value))
+            if (!any(is.na(years))) {
+                # We have a valid list of numbers
+                # Check if consequtive:
+                if (all(diff(years) == 1)) {
+                    # If so, we're done
+                    rv = years
+                }
+            }
+        }
+    } 
+    # Double or Integer?
+    else if (type == "double" || type == "integer") {
+        if (entry.length == 1) {
+            # It's a single number / vector, just return it
+            rv = c(year.value)
+        } else {
+            # It's a multi number vector, check if consequtive     
+            if (all(diff(year.value) == 1)) {
+                rv = year.value
+            }
+        }
+    }
+    # List?
+    else if (type == "list") {
+        # Unsure if we want this supported as of now
+    }
+
+    rv
 }
 
 get.age.bounds.for.age.value <- function(surveillance.manager,
