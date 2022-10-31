3accf94bedb10ec3879ea204bf4890137474c2b2
JPennington <jpennin6@jhu.edu>
2022-10-31 12:18:00 -0400

    Moved comments arounds

--- R/data_managers/surveillance_data_manager.R
+++ R/data_managers/surveillance_data_manager.R
@@ -21,6 +21,24 @@ get.surveillance.data <- function(surveillance.data.manager,
 #' return.as.vectors
 #'
 #' Given some input, attempt to return it as a list of double vectors.
+#' I believe this should take a value and convert it into a vector representation
+#' So if there is only one entry, we return it as c(entry).
+#' This way we have a consistent return value, a vector of values (possibly none, if not
+#' a year)
+#' 
+#'   for example
+#'     year.value = 2020 or '2020' or c(2020) -> c(2020)
+#'     year.value = "2018-2020" --> c(2018,2019,2020)
+#'     year.value = 2018:2020 --> c(2018,2019,2020)
+#'     year.value = c("2018","2019") --> c(2018,2019)
+#'     year.value = c(2018, 2019, 2020) --> c(2018, 2019, 2020)
+#'
+#' If this function is unable to determine what years are being
+#' asked for it will return NA
+#'
+#' We only allow consequtive years, but we allow forwards or backwards.
+#' We assume that the numbers themselves are valid years, and we allow 
+#' both integers and doubles
 #'
 #' @param input The input to the function; either already doubles/integers or characters
 #'
@@ -87,27 +105,6 @@ return.as.vectors <- function ( input ) {
 #'
 #' Takes input that can be represented as a sequential list of years and returns
 #' the years that are represented by this input.
-#'
-#' I believe this should take a value and convert it into a vector representation
-#' So if there is only one entry, we return it as c(entry).
-#' This way we have a consistent return value, a vector of values (possibly none, if not
-#' a year)
-#' 
-#'   for example
-#'     year.value = 2020 or '2020' or c(2020) -> c(2020)
-#'     year.value = "2018-2020" --> c(2018,2019,2020)
-#'     year.value = 2018:2020 --> c(2018,2019,2020)
-#'     year.value = c("2018","2019") --> c(2018,2019)
-#'     year.value = c(2018, 2019, 2020) --> c(2018, 2019, 2020)
-#'
-#' If this function is unable to determine what years are being
-#' asked for it will return NA
-#'
-#' We only allow consequtive years, but we allow forwards or backwards.
-#' We assume that the numbers themselves are valid years, and we allow 
-#' both integers and doubles
-#'
-#' This functionality has been reproduced in the more generic function return.as.vectors
 #'
 #' @param surveillance.manager A reference to the usable surveillance.manager (needed?)
 #' @param year.value Some value representing which years we would like to examine
