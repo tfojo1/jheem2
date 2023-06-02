ce7a056e6bbcaf20cc8b90da950ad636385ee2a5
tfojo1 <anthony.fojo@jhmi.edu>
2023-06-02 12:52:49 -0400

    setting up code for building package to be installable

---
+++ code_for_building_package/run_before_pushing_installable_package_code_to_github.R
@@ -0,0 +1,53 @@
+
+##--------------------------------------------------------------------------------------------##
+##--                                     OVERVIEW                                           --##
+##--------------------------------------------------------------------------------------------##
+##
+## Source this file prior to pushing an 'installable' package up to GitHub
+## It will take care of updating documentation and internally-cached data structures
+##
+## This depends on packages
+## - usethis
+## - devtools
+## Which do NOT have to be imported by the package itself (just used to set up the package)
+##
+##--------------------------------------------------------------------------------------------##
+
+
+##-----------------------------------------------------------##
+##-- STEP 1: Update Documentation (updates NAMESPACE file) --##
+##-----------------------------------------------------------##
+
+cat("\n-----------------------------\nSETTING UP DOCUMENTATION...\n")
+devtools::document(quiet=T)
+cat("DONE SETTING UP DOCUMENTATION\n-----------------------------\n")
+
+
+##-----------------------------------------------------------------##
+##-- STEP 2: Indicate Which Files the Package should NOT include --##
+##-----------------------------------------------------------------##
+
+cat("\n------------------------------------\nSETTING FILES TO IGNORE IN BUILD...\n")
+usethis::use_build_ignore(files=c("^R\\tests$", 
+                                  "^data-raw$",
+                                  "^\\.github$",
+                                  "^.*\\.Rproj$"),
+                          escape=F)
+cat("DONE SETTING FILES TO IGNORE IN BUILD\n------------------------------------\n")
+
+##---------------------------------------------------------##
+##-- STEP 3: Build and Store the Cached Location Manager --##
+##---------------------------------------------------------##
+
+cat("\n------------------------------\nBUILDING LOCATION MANAGER...\n")
+# In real life: source the file that builds it
+# For now, just build a test one
+LOCATION.MANAGER = new.env()
+
+# Store it to an internal file for the package
+usethis::use_data(LOCATION.MANAGER, internal = T)
+
+cat("DONE BUILDING LOCATION MANAGER\n------------------------------\n")
+
+
+cat("\n\n ALL DONE PREPARING PACKAGE TO BE INSTALLED\n")
