3feaf2fdf538888a014611a3da1817647540cffb
JPennington <jpennin6@jhu.edu>
2023-06-02 12:42:32 -0400

    Slight fix to the size of the R6 location object
    Don't need the class or clone attributes on the objects; they
    are created and then kept, and we don't have any inheritance.
    
    This reduces the size of the resulting RDS file from 18.6 MB to
    7.8 MB on my machine, and results in a much smaller generation and file
    save time (total time around 5 minutes on my machine).

--- R/LOCATIONS_location_manager.R
+++ R/LOCATIONS_location_manager.R
@@ -13,28 +13,6 @@
 
 location.manager.cached.filename = "locations/Cached.Location.Manager.rds"
 
-if (!exists("LOCATION.MANAGER")) {
-  # The LOCATION.MANAGER is NOT resident in memory.
-  # Does the cached file exist?
-  if (file.exists(location.manager.cached.filename)) {
-    # The file exists, load it into memory
-    cat(sprintf("Loading LOCATION.MANAGER from %s...", location.manager.cached.filename))
-    assign ("LOCATION.MANAGER", readRDS(location.manager.cached.filename), envir = .GlobalEnv)
-    cat("Done\n")
-  } else {
-    # The file had not yet been cached
-    # The first file loads the implementation of the LOCATION.MANAGER
-    # The second file loads the location data into the LOCATION.MANAGER and then
-    # saves the cached file to disk
-    source ("R/LOCATIONS_impl.R")
-    cat("Loading location data into the LOCATION.MANAGER...")
-    source ("R/LOCATIONS_cache.R")
-    cat("Done\n")
-  }
-} else {
-  # LOCATION.MANAGER is resident in memory
-  cat("LOCATION.MANAGER already loaded\n")
-}
 
 ##-------------##
 ##-- Getters --##
@@ -353,4 +331,27 @@ register.sub.and.super.locations <- function(sub.locations,
     super.completely.encloses.sub = rep(super.completely.encloses.sub,length(sub.locations))
   }
   LOCATION.MANAGER$register.hierarchy(sub.locations, super.locations, super.completely.encloses.sub) 
+}
+
+if (!exists("LOCATION.MANAGER")) {
+  # The LOCATION.MANAGER is NOT resident in memory.
+  # Does the cached file exist?
+  if (file.exists(location.manager.cached.filename)) {
+    # The file exists, load it into memory
+    cat(sprintf("Loading LOCATION.MANAGER from %s...", location.manager.cached.filename))
+    assign ("LOCATION.MANAGER", readRDS(location.manager.cached.filename), envir = .GlobalEnv)
+    cat("Done\n")
+  } else {
+    # The file had not yet been cached
+    # The first file loads the implementation of the LOCATION.MANAGER
+    # The second file loads the location data into the LOCATION.MANAGER and then
+    # saves the cached file to disk
+    source ("R/LOCATIONS_impl.R")
+    cat("Loading location data into the LOCATION.MANAGER...")
+    source ("R/LOCATIONS_cache.R")
+    cat("Done\n")
+  }
+} else {
+  # LOCATION.MANAGER is resident in memory
+  cat("LOCATION.MANAGER already loaded\n")
 }
