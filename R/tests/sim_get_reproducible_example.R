e71a4be6ec89b41807facd6d78146318438e026f
A-Zalesak <azalesak@alumni.unc.edu>
2023-08-31 12:15:48 -0400

    Add to example of sim$get issue

--- R/tests/sim_get_reproducible_example.R
+++ R/tests/sim_get_reproducible_example.R
@@ -16,3 +16,14 @@ sim.numerator.data = sim$get(outcome='new',
 
 # Error in array.access(private$i.data[[outcome]], dimension.values) :
 #     Error in array.access(): Elements of ... which are lists must be NAMED
+
+
+sim.denominator.data = sim$get(outcome='population',
+                               keep.dimensions = c('year', 'sex'))
+# Error getting simulation results: For the 'population' outcome, 'dimension 'year' must have dimension.values specified (it is NULL in the ontology and must be specified in the get() call)
+
+sim.denominator.data = sim$get(outcome='population',
+                               keep.dimensions = c('year', 'sex'),
+                               dimension.values = list(year=as.character(2008:2021)))
+# Invalid values fir dimension 'year' in 'dimension.values': '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'
+# Problem traces to check.array.access.arguments(arr = arr, dimension.values = dimension.values)
