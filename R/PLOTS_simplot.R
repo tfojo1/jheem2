a62fc64f6ad526830aaec5fd2241d14e438d859a
A-Zalesak <azalesak@alumni.unc.edu>
2023-09-21 14:31:10 -0400

    Implement simple working version of simplot

--- R/PLOTS_simplot.R
+++ R/PLOTS_simplot.R
@@ -3,8 +3,8 @@ sim = make.dummy.sim()
 
 #'@param ... One or more of either (1) jheem.simulation objects or (2) jheem.simset objects or (3) lists containing only jheem.simulation or jheem.simset objects
 #'@param outcomes A character vector of which simulation outcomes to plot
-#'@param split.by
-#'@param facet.by
+#'@param split.by AZ: at most one dimension
+#'@param facet.by AZ: any number of dimensions but cannot include the split.by dimension
 #'@param dimension.values
 #'@param data.manager The data.manager from which to draw real-world data for the plots
 #'@param style.manager We are going to have to define this down the road. It's going to govern how we do lines and sizes and colors. For now, just hard code those in, and we'll circle back to it
@@ -17,18 +17,36 @@ sim = make.dummy.sim()
 #'@export
 simplot <- function(...,
                     outcomes,
-                    split.by,
-                    facet.by = character(),
-                    dimension.values,
+                    split.by = NULL,
+                    facet.by = NULL,
+                    dimension.values = list(),
                     data.manager,
                     style.manager)
 {
+    ### --- FEATURES TO ADD --- ###
+    # - change y-axis from 'value' to the name of the outcome? Maybe not, since "value" is shared by all plots
+    # - allow changing color scheme for lines and points
+    
+    # -- VALIDATION -- #
+    
+    error.prefix = "Cannot generate simplot: "
+    
+    # *split.by* is NULL or a single, non-NA character vector
+    if (!is.null(split.by) && (!is.character(split.by) || length(split.by) > 1 || is.na(split.by)))
+        stop(paste0(error.prefix, "'split.by' must be NULL or a length one, non-NA character vector"))
+    
+    # *facet.by* is NULL or a character vector of length > 0 with no NAs or duplicates
+    if (!is.null(facet.by) && (!is.character(facet.by) || length(facet.by) < 1 || any(is.na(facet.by)) || any(duplicated(facet.by))))
+        stop(paste0(error.prefix, "'facet.by' must be NULL or a character vector with at least one element and no NAs or duplicates"))
+    
     #-- STEP 1: PRE-PROCESSING --#
     # Get a list out of ... where each element is one simset (or sim for now)
-    browser()
     sim.list = list(...) # will later be SIMSETS
-    # @Andrew - fill in
     # - make sure they are all the same version and the location
+    if (length(unique(sapply(sim.list, function(sim) {sim$version}))) > 1)
+        stop(paste0(error.prefix, "all simulations must have the same version"))
+    if (length(unique(sapply(sim.list, function(sim) {sim$location}))) > 1)
+        stop(paste0(error.prefix, "all simulations must have the same location"))
     
     # Check outcomes
     # - make sure it's a non-empty character vector, no NAs
@@ -57,74 +75,107 @@ simplot <- function(...,
                 break
             } else i = i + 1
         }
+        # if (is.null(corresponding.observed.outcome))
+        #     stop(paste0("No corresponding observed outcome found for outcome '", outcome, "'")) # Shouldn't happen
         corresponding.observed.outcome
     })
-    ## HARDCODE BECAUSE CORRESPONDING.OBSERVED.OUTCOME INFORMATION IS MISSING
-    outcomes.for.data = c('diagnoses')
     
-    if (any(sapply(outcomes.for.data, is.null)))
-        stop("No corresponding observed outcomes for one or more simulation outcomes in 'outcomes'") # Shouldn't happen
-    outcomes.for.data = setNames(outcomes.for.data, outcomes)
+    ## HARDCODE BECAUSE CORRESPONDING.OBSERVED.OUTCOME INFORMATION IS MISSING
+    outcomes.for.data = setNames(c('diagnoses', 'prevalence'), outcomes)
+    
+    outcome.ontologies = lapply(outcomes, function(outcome) {
+        outcome.ontology = NULL
+        i = 1
+        while (i <= length(sim.list)) {
+            if (outcome %in% names(sim.list[[i]]$outcome.ontologies)) {
+                outcome.ontology = sim.list[[i]]$outcome.ontologies[[outcome]]
+                break
+            } else i = i + 1
+        }
+        if (is.null(outcome.ontology))
+            stop(paste0("No outcome ontology found for outcome '", outcome, "'")) # Shouldn't happen
+        outcome.ontology
+    })
     
     # Get the locations to pull data for for each outcome
     # - for now, just use the sim$location
     location = sim.list[[1]]$location
     
     #-- STEP 2: MAKE A DATA FRAME WITH ALL THE REAL-WORLD DATA --#
-    # columns:
-    # $year
-    # $value
-    # $outcome
-    # $split
-    # $sim.name - name for this simulation
-    # <a column for each element of facet.by>
-    
-    
+
+    outcome.mappings = list()
     
     df.truth = NULL
-    # I probably want to make sure this data frame is still indexed by simulation outcome
-    for (outcome in outcomes.for.data)
+    for (i in seq_along(outcomes.for.data))
     {
-        outcome.data = data.manager$pull(outcome = outcomes.for.data[[outcome]],
-                                         dimension.values = dimension.values,
-                                         keep.dimensions = union('year', facet.by))
-        one.df.outcome = reshape2::melt(outcome.data) # I need to add an outcome column, don't I?
+        outcome.data = data.manager$pull(outcome = outcomes.for.data[[i]],
+                                         dimension.values = c(dimension.values, list(location = location)),
+                                         keep.dimensions = c('year', facet.by, split.by), #'year' can never be in facet.by
+                                         target.ontology = outcome.ontologies[[i]],
+                                         allow.mapping.from.target.ontology = T)
+        outcome.mappings = c(outcome.mappings, list(attr(outcome.data, 'mapping')))
+        one.df.outcome = reshape2::melt(outcome.data, na.rm = T)
+        corresponding.outcome = names(outcomes.for.data)[[i]]
+        one.df.outcome['outcome'] = corresponding.outcome
         df.truth = rbind(df.truth, one.df.outcome)
     }
-    df.truth[[outcome]] = lapply(df.truth[[outcome]], function(x){outcome.mapping[[x]]}) # probably incorrect syntax
-    
-    
+    names(outcome.mappings) = outcomes
+
     #-- STEP 3: MAKE A DATA FRAME WITH ALL THE SIMULATIONS' DATA --#
-    # columns:
-    # $year
-    # $value
-    # $outcome
-    # $split
-    # $sim.name - name for the simulation
-    # <a column for each element of facet.by>
-    
     
     df.sim = NULL
-    for (sim in sims)
+    for (i in seq_along(sim.list))
     {
+        sim = sim.list[[i]]
         sim.data = sim$get(outcomes = outcomes,
                            dimension.values = dimension.values,
-                           keep.dimensions = union('year', facet.by),
+                           keep.dimensions = c('year', facet.by, split.by),
                            drop.single.outcome.dimension = F)
+
+        dim.names.without.outcome = dimnames(sim.data)[names(dimnames(sim.data)) != 'outcome']
+        outcome.arrays = lapply(outcomes, function(outcome) {
+            arr = array(sim.data[get.array.access.indices(dimnames(sim.data),
+                                                          dimension.values = list(outcome = outcome))],
+                        dim = sapply(dim.names.without.outcome, length),
+                        dimnames = dim.names.without.outcome)
+            outcome.mappings[[outcome]]$apply(arr)
+        })
+        mapped.dimnames = c(dimnames(outcome.arrays[[1]]), list(outcome = outcomes))
+        mapped.sim.data = array(unlist(outcome.arrays), dim=sapply(mapped.dimnames, length), dimnames = mapped.dimnames)
         
-        one.df.sim = reshape2::melt(sim.data)
+        one.df.sim = reshape2::melt(mapped.sim.data, na.rm = T)
+        one.df.sim['sim.name'] = i
         
         df.sim = rbind(df.sim, one.df.sim)
     }
-    
     
     #- STEP 4: MAKE THE PLOT --#
     
     facet.formula = as.formula(paste0("~outcome",
                                       paste0(" + ", facet.by, collapse='')))
     
-    ggplot() +
-        geom_line(data=df.sim, aes(x=year, y=value, color=sim.name)) +
-        geom_point(data=df.truth, aes(x=year, y=value)) +
-        facet_wrap(facet.formula, scales = 'free_y')
+    if (is.null(split.by)) {
+        if (is.null(facet.by)) {
+            ggplot() +
+                geom_line(data=df.sim, aes(x=year, y=value, color=sim.name)) +
+                geom_point(data=df.tuth, aes(x=year, y=value))
+        } else {
+            ggplot() +
+                geom_line(data=df.sim, aes(x=year, y=value, color=sim.name)) +
+                geom_point(data=df.truth, aes(x=year, y=value)) +
+                facet_wrap(facet.formula, scales = 'free_y')
+        }
+    } else {
+        if (is.null(facet.by)) {
+            ggplot() +
+                geom_line(data=df.sim, aes(x=year, y=value, color=!!sym(split.by))) +
+                geom_point(data=df.truth, aes(x=year, y=value, color=!!sym(split.by)))
+        } else {
+            ggplot() +
+                geom_line(data=df.sim, aes(x=year, y=value, color=!!sym(split.by))) +
+                geom_point(data=df.truth, aes(x=year, y=value, color=!!sym(split.by))) +
+                facet_wrap(facet.formula, scales = 'free_y')
+        }
+        
+    }
 }
---
+++ R/tests/simplot_test.R
@@ -0,0 +1,17 @@
+
+source('../jheem_analyses/applications/EHE/ehe_specification.R')
+source('R/tests/make_dummy_sim.R')
+source('R/PLOTS_simplot.R')
+surveillance.manager = load.data.manager('../jheem_analyses/cached/surveillance.manager_080923.rdata')
+
+
+my.simplot = simplot(sim=make.dummy.sim(version='ehe',
+                                        location='MD',
+                                        from.year=2006,
+                                        to.year=2023),
+                     outcomes=c('new', 'incidence'), # remember to add in the hard-coded corresponding observed outcomes in PLOTS_simplot.R
+                     split.by = 'risk',
+                     facet.by = c('sex'),
+                     dimension.values = list(), #c(sex='female'), Why did Todd have this as a vector?
+                     data.manager = surveillance.manager,
+                     style.manager = NULL)
