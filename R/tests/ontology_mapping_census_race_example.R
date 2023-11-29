29cd5b1ad08fcbe9b64101b35b6ac18a14864ba8
A-Zalesak <azalesak@alumni.unc.edu>
2023-11-29 11:45:08 -0500

    Add example of possible ontology mapping bug with census to jheem race mapping

---
+++ R/tests/ontology_mapping_census_race_example.R
@@ -0,0 +1,5 @@
+mp = ONTOLOGY.MAPPING.MANAGER$mappings$census.to.jheem.race
+x = mp$from.dim.names
+y = mp$to.dim.names
+get.ontology.mapping(x, y) # NULL
+get.mappings.to.align.ontologies(x, y) # NULL
