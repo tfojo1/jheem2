76e7a7d3384d9f79a1f37f587ed2fbbc54c3d414
A-Zalesak <azalesak@alumni.unc.edu>
2024-01-11 13:28:00 -0500

    Reprex of single years mapped to smaller year ranges

--- R/tests/ontology_mapping_year_ranges_example.R
+++ R/tests/ontology_mapping_year_ranges_example.R
@@ -1,7 +1,10 @@
 # These ontologies represent what happens with the new migration data
 ont1 = ontology(year='2011-2015', incomplete.dimensions = 'year')
-ont2 = ontology(year=as.character(2011:2015), incomplete.dimensions = 'year')
+# ont2 = ontology(year=as.character(2011:2015), incomplete.dimensions = 'year')
+# 
+# # I expected both of these to work in this case, but one doesn't
+# get.mappings.to.align.ontologies(ont1, ont2, allow.non.overlapping.incomplete.dimensions = F) # an identity and a basic (correct)
+# get.mappings.to.align.ontologies(ont1, ont2, allow.non.overlapping.incomplete.dimensions = T) # two identities (incorrect)
 
-# I expected both of these to work in this case, but one doesn't
-get.mappings.to.align.ontologies(ont1, ont2, allow.non.overlapping.incomplete.dimensions = F) # an identity and a basic (correct)
-get.mappings.to.align.ontologies(ont1, ont2, allow.non.overlapping.incomplete.dimensions = T) # two identities (incorrect)
+ont3 = ontology(years=as.character(2011:2020), incomplete.dimensions = 'year')
+get.ontology.mapping(ont3, ont1) #NULL
