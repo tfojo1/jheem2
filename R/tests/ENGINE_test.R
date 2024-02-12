79159f03158f4dd6c83367583165c7ca3c700592
Todd Fojo <tfojo1@jh.edu>
2024-02-12 13:42:48 -0500

    updated default engine test global trate

--- R/tests/ENGINE_test.R
+++ R/tests/ENGINE_test.R
@@ -4,7 +4,7 @@ print("ENGINE TEST: Creating Specification")
 source('../jheem_analyses/applications/EHE/ehe_specification.R')
 
 params = suppressWarnings(get.medians(EHE.PARAMETERS.PRIOR))
-params['global.trate'] = 0.1
+params['global.trate'] = 0.075
 
 
 print("ENGINE TEST: Setting up Engine")
