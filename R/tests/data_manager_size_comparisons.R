997c687fe2d73081e901ae1549ef3d9e087dc0f1
Todd Fojo <tfojo1@jh.edu>
2024-03-15 13:08:18 -0400

    more size comparisons

--- R/tests/data_manager_size_comparisons.R
+++ R/tests/data_manager_size_comparisons.R
@@ -8,3 +8,9 @@ new.elem.size = sapply(SURVEILLANCE.MANAGER, object.size)
 
 delta.size = new.elem.size - orig.elem.size; delta.size = sort(delta.size, decreasing = T); delta.size
 size.ratio = new.elem.size / orig.elem.size; size.ratio = size.ratio[names(delta.size)]; size.ratio
+
+orig.size = sum(orig.elem.size)
+new.size = sum(new.elem.size)
+extra.size.mb = (new.size - orig.size) / 1000000; extra.size.mb
+extra.size.ratio = new.size/orig.size; extra.size.ratio
+
