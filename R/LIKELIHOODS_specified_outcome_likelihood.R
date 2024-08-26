acac9e0cb843313d486cf1d4183cbc6836d7b747
A-Zalesak <azalesak@alumni.unc.edu>
2024-08-26 07:37:31 -0400

    comment out unused code

--- R/LIKELIHOODS_specified_outcome_likelihood.R
+++ R/LIKELIHOODS_specified_outcome_likelihood.R
@@ -14,12 +14,12 @@ create.specified.outcome.likelihood.instructions <- function(outcome.for.sim,
 
 
 
-dep.ratio = array(c(3,3.2,3.1),
-                  dim = c(race=3),
-                  dimnames = list(race=c('black','hispanic','white','other')))
-
-dep.ratio.total.for.several.years.instr = create.specified.outcome.likelihood.instructions(outcome.for.sim = 'depression.prevalence.ratio',
-                                                                                           outcome.value = 3.1,
-                                                                                           additional.dimension.values = list(year=2010:2020,
-                                                                                                                              sex=c('male','female'))
-)
+# dep.ratio = array(c(3,3.2,3.1),
+#                   dim = c(race=3),
+#                   dimnames = list(race=c('black','hispanic','white','other')))
+# 
+# dep.ratio.total.for.several.years.instr = create.specified.outcome.likelihood.instructions(outcome.for.sim = 'depression.prevalence.ratio',
+#                                                                                            outcome.value = 3.1,
+#                                                                                            additional.dimension.values = list(year=2010:2020,
+#                                                                                                                               sex=c('male','female'))
+# )
