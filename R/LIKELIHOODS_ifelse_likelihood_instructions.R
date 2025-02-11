2925791e6ac73a65b78c59594073fb583e800113
A-Zalesak <azalesak@alumni.unc.edu>
2025-02-11 14:07:27 -0500

    complete refactor of ifelse likelihood and create wrapper for location based ifelse

--- R/LIKELIHOODS_ifelse_likelihood_instructions.R
+++ R/LIKELIHOODS_ifelse_likelihood_instructions.R
@@ -1,11 +1,39 @@
-create.ifelse.likelihood.instructions <- function(..., FUN=get.default.ifelse.function()) {
+#' @title Create If-Else Likelihood Instructions
+#' @description
+#' Create instructions that can try a sequence of likelihood instructions until the first one that works without error.
+#' 
+#' @param ... 'jheem.likelihood.instructions' objects that all have the same 'outcome.for.sim'.
+#' @export
+create.ifelse.likelihood.instructions <- function(...) {
+    do.create.ifelse.likelihood.instructions(list(...), FUN=get.default.ifelse.function())
+}
+
+#' @title Create Location Based If-Else Likelihood Instructions
+#' @description
+#' Create instructions that can instantiate a different 'jheem.likelihood' depending on the location.
+#' 
+#' @inheritParams create.ifelse.likelihood.instructions
+#' @param locations.list A list of character vectors of locations that will use the corresponding instruction.
+#' The list should be length one shorter than the number of instructions. For example, if you have three instructions,
+#' the first vector has locations that will use the first instruction and second vector has locations that will
+#' use the second instruction. The third instruction will be used for all other locations.
+#' @export
+create.location.based.ifelse.likelihood.instructions <- function(..., locations.list=list()) {
+    error.prefix <- "Error creating 'jheem.ifelse.likelihood.instructions': "
+    sub.instructions <- list(...)
+    if (!is.list(locations.list) || length(locations.list)!=length(sub.instructions)-1 ||
+        any(sapply(locations.list, function(v) {!is.character(v)})))
+        stop(paste0(error.prefix, "'locations.list' must be a list of character vectors with length one less than the number of instructions in '...'"))
+    do.create.ifelse.likelihood.instructions(sub.instructions, FUN=get.location.based.ifelse.function(locations.list))
+}
+
+do.create.ifelse.likelihood.instructions <- function(sub.instructions, FUN=get.default.ifelse.function()) {
     error.prefix <- "Error creating 'jheem.ifelse.likelihood.instructions': "
     # all arguments must be JHEEM.LIKELIHOOD.INSTRUCTIONS and none joint
-    sub.instructions <- list(...)
     if (any(sapply(sub.instructions, function(x) {
         !R6::is.R6(x) || !is(x, "jheem.likelihood.instructions") || is(x, "jheem.joint.likelihood.instructions")
     }))) {
-        stop(paste0(error.prefix, "all arguments must be 'jheem.likelihood.instructions' objects and none may 'jheem.joint.likelihood.instructions' objects"))
+        stop(paste0(error.prefix, "all arguments in '...' must be 'jheem.likelihood.instructions' objects and none may be 'jheem.joint.likelihood.instructions' objects"))
     }
     
     # all outcomes for sim must be identical
@@ -19,38 +47,6 @@ create.ifelse.likelihood.instructions <- function(..., FUN=get.default.ifelse.fu
     # FUN must take certain arguments
     JHEEM.IFELSE.LIKELIHOOD.INSTRUCTIONS$new(sub.instructions = sub.instructions,
                                              FUN = FUN)
-}
-
-#' @export
-get.default.ifelse.function <- function() {
-    function(sub.instructions,
-             version,
-             location,
-             sub.version=NULL,
-             data.manager=get.default.data.manager(),
-             verbose=F,
-             throw.error.if.no.data=F,
-             error.prefix = "Error instantiating likelihood from instructions: ") {
-        resulting.likelihood = NULL
-        for (sub.instr in sub.instructions) {
-            resulting.likelihood <- tryCatch(
-                {
-                    do.instantiate.likelihood(instructions = sub.instr,
-                                              version = version,
-                                              location = location,
-                                              sub.version = sub.version,
-                                              data.manager = data.manager,
-                                              additional.weights = list(),
-                                              throw.error.if.no.data = throw.error.if.no.data,
-                                              verbose = verbose,
-                                              error.prefix = error.prefix)
-                },
-                error = NULL)
-            if (!is.null(resulting.likelihood)) return(resulting.likelihood)
-        }
-        if (is.null(resulting.likelihood))
-            stop(paste0(error.prefix, "all alternative likelihoods failed"))
-    }
 }
 
 JHEEM.IFELSE.LIKELIHOOD.INSTRUCTIONS <- R6::R6Class(
@@ -98,7 +94,7 @@ JHEEM.IFELSE.LIKELIHOOD.INSTRUCTIONS <- R6::R6Class(
                                                  verbose = verbose,
                                                  error.prefix = error.prefix)
             
-            ## TO DO: ERROR CHECK HERE
+            ## TO DO: ERROR CHECK HERE... if I want to let people make their own (not now)
             
             resulting.likelihood
         }
@@ -124,3 +120,59 @@ JHEEM.IFELSE.LIKELIHOOD.INSTRUCTIONS <- R6::R6Class(
         i.FUN = NULL
     )
 )
+
+get.default.ifelse.function <- function() {
+    function(sub.instructions,
+             version,
+             location,
+             sub.version=NULL,
+             data.manager=get.default.data.manager(),
+             verbose=F,
+             throw.error.if.no.data=F,
+             error.prefix = "Error instantiating likelihood from instructions: ") {
+        resulting.likelihood = NULL
+        for (sub.instr in sub.instructions) {
+            resulting.likelihood <- tryCatch(
+                {
+                    do.instantiate.likelihood(instructions = sub.instr,
+                                              version = version,
+                                              location = location,
+                                              sub.version = sub.version,
+                                              data.manager = data.manager,
+                                              additional.weights = list(),
+                                              throw.error.if.no.data = throw.error.if.no.data,
+                                              verbose = verbose,
+                                              error.prefix = error.prefix)
+                },
+                error = NULL)
+            if (!is.null(resulting.likelihood)) return(resulting.likelihood)
+        }
+        if (is.null(resulting.likelihood))
+            stop(paste0(error.prefix, "all alternative likelihoods failed"))
+    }
+}
+
+get.location.based.ifelse.function <- function(locations.list) {
+    function(sub.instructions,
+             version,
+             location,
+             sub.version=NULL,
+             data.manager=get.default.data.manager(),
+             verbose=F,
+             throw.error.if.no.data=F,
+             error.prefix = "Error instantiating likelihood from instructions: ") {
+        
+        location.in.list <- sapply(locations.list, function(x) {location %in% x})
+        which.instr.to.use <- which(location.in.list)
+        if (length(which.instr.to.use)==0) which.instr.to.use <- length(sub.instructions)
+        do.instantiate.likelihood(instructions=sub.instructions[[which.instr.to.use]],
+                                  version = version,
+                                  location = location,
+                                  sub.version = sub.version,
+                                  data.manager = data.manager,
+                                  additional.weights = list(),
+                                  throw.error.if.no.data = throw.error.if.no.data,
+                                  verbose = verbose,
+                                  error.prefix = error.prefix)
+    }
+}
