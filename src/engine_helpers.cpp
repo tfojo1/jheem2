5c031473fa4e5c060f73d321f343a5f619e21042
Todd Fojo <tfojo1@jh.edu>
2025-03-05 12:49:30 -0500

    removed error (old debug line) from apply_foregrounds

--- src/engine_helpers.cpp
+++ src/engine_helpers.cpp
@@ -637,8 +637,6 @@ List apply_foregrounds(List values,
                 // 2) An inner loop (over i_index/i) that goes through all the value times for which this effect time j applies
                 while (j <= n_effect_times)
                 {
-                    if (j > 1)
-                        return(List::create());
                     // Rcout << "**** j = " << j << ", time_before = " << time_before << ", time_after = " << time_after << "\n";
                     // at the start of this loop, we know that
                     //   value_times[i] >= time_before
